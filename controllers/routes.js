8bdbaa60b263be1ce79564fb7a9fc70dd0ee138b
Rafael <rafaeldajuda@gmail.com>
2021-08-08 12:49:59 -0300

    correcao routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,14 +4,14 @@ const httpRequest = require('../controllers/httpRequest');
 const PrevisaoTempo = require('../model/previsaoTempo');
 const path = require('path');
 //const pathViews = '/home/rafael/Documentos/Outros/APIs Node/previsao-temperatura/'
-const pathViews = '~/previsao-temperatura/'
+const pathViews = '/app/'
 
 module.exports = (app) => {
     app.get('/', (req, res) =>{
         //res.sendFile(path.join(__dirname + '/index.html'));
         //res.sendFile(pathViews + "index.html");
 
-        res.status(200).sendFile(__dirname + "/index.html");
+        res.status(200).sendFile(__dirname + "index.html");
     });
 
     app.get('/temperaturas', (req, res) =>{
